0ac96338567dcdfc24136294d1244074f5c0e91a
Kirill Glukhov <valeralunin34@gmail.com>
2024-05-28 01:34:36 +0700

    New commit

---
+++ src/MainPart.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function MainPart() {
+    return <div>Main Part Component</div>;
+}
+
+export default MainPart;
---
+++ src/app.jsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import MainPart from './MainPart.jsx';
+
+const root = createRoot(document.getElementById('root'));
+root.render(<MainPart />);
